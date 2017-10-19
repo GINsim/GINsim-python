a92d1ba61f2087f7c92792b429be1cd7cbf83868
Aurelien Naldi <aurelien.naldi@gmail.com>
2017-10-19 11:29:00 +0200

    When running in the notebook, dd a ginsim.show() method to display the LRG inline

--- ginsim/__init__.py
+++ ginsim/__init__.py
@@ -5,6 +5,8 @@ from .jupyter import upload
 
 from .gateway import japi, restart
 
+import colomoto_jupyter
+
 class GINsim(object):
     def restart(self):
         restart()
@@ -14,6 +16,11 @@ class GINsim(object):
 
     def __getattr__(self, name):
         return getattr(japi.gs, name)
+
+    if colomoto_jupyter.IN_IPYTHON:
+        def show(self, lrg):
+            b64 = japi.gs.service("image").base64PNG(lrg)
+            return colomoto_jupyter.show_image(b64)
 
 sys.modules[__name__] = GINsim()
 
