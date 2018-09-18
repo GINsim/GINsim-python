700690698ce5d7c7605b7fd345188747c6cb6ef1
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-09-18 15:41:29 +0200

    ginsim_setup: add clingo

--- ginsim_setup.py
+++ ginsim_setup.py
@@ -1,3 +1,5 @@
 from colomoto.setup_helper import setup
 setup({"pkg": "colomoto/ginsim",
-        "check_progs": ["GINsim"]})
+        "check_progs": ["GINsim"]},
+    {"pkg": "potassco/clingo",
+        "check_progs": ["clingo"]})
