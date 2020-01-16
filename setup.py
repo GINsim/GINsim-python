daa73230611cf163b36ca9301d739ab1c784f2bb
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-01-16 14:40:57 +0100

    release v0.3.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 NAME = 'ginsim'
 
 setup(name=NAME,
-    version='0.3.8',
+    version='0.3.9',
     author = "Aurélien Naldi",
     author_email = "aurelien.naldi@gmail.com",
     url = "https://github.com/GINsim/GINsim-python",
